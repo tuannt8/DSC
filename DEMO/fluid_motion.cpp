273b4d7dd79f14cda0403c80b43f96a3bf4bb6e4
tuannt8 <tntr@git.compute.dtu.dk>
2018-01-29 15:16:49 +0100

    More log detail

--- DEMO/fluid_motion.cpp
+++ DEMO/fluid_motion.cpp
@@ -85,6 +85,8 @@ void fluid_motion::deform()
 
 void fluid_motion::project_interface()
 {
+    std::cout << "Project interface \n";
+    double max_projection = 0;
     for (auto nit = s_dsc->nodes_begin(); nit != s_dsc->nodes_end(); nit++)
     {
         if (nit->is_interface())
@@ -98,8 +100,15 @@ void fluid_motion::project_interface()
             }
             vec3 new_pos = nit->get_pos() + norm*t;
             s_dsc->set_destination(nit.key(), new_pos);
+            
+            if(max_projection < std::abs(t))
+            {
+                max_projection = t;
+            }
         }
     }
+    
+    std::cout << "Max projection: " << max_projection << std::endl;
     
     s_dsc->deform();
 }
