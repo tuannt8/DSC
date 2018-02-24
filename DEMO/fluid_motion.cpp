9ec22980a4214b22f08371480391d30f40644577
tuannt8 <tntr@git.compute.dtu.dk>
2018-02-24 07:33:09 +0100

    Change adaptive time scheme

--- DEMO/fluid_motion.cpp
+++ DEMO/fluid_motion.cpp
@@ -33,7 +33,7 @@ void fluid_motion::init(DSC::DeformableSimplicialComplex<> *dsc){
     s_dsc = dsc;
     m_max_dsc_displacement = s_dsc->get_avg_edge_length()*0.3;
     
-    m_max_displacement_projection = m_problem->m_deltap*0.3; // This is just a compliment
+    m_max_displacement_projection = m_problem->m_deltap*0.4; // This is just a compliment
     
     cout << "\n\n+++++++++++++++++++++++++++++++++++++++"
     << "\n spacing distance: " << m_problem->m_deltap
@@ -156,22 +156,27 @@ void fluid_motion:: advect_velocity()
         }
     }
     
-//    // Update adaptive time step
-//    if (m_max_dsc_displacement < max_dis)
-//    {
-//        dt = dt*m_max_dsc_displacement / max_dis;
-//        // time step only reduce
-//    }
-//
+    // Update adaptive time step
+    static vector<double> grad_max_dis;
+    double current_grad = max_dis / dt;
+    grad_max_dis.push_back(current_grad);
+    if (grad_max_dis.size() > 10)
+    {
+        grad_max_dis.erase(grad_max_dis.begin());
+    }
+    // estimate new dt
+    if(grad_max_dis.size() > 1)
+    {
+        double average_grad = std::accumulate(grad_max_dis.begin(), grad_max_dis.end(), 0.0) / grad_max_dis.size();
+        dt = m_max_dsc_displacement / average_grad;
+    }
+    // End update time step
+
     cout << "Max advection: " << max_dis << endl;
 
     snapp_boundary_vertices();
 
     int step = s_dsc->deform(20);
-    if (step > 6)
-    {
-        dt = dt * 6.0 / step;
-    }
 }
 
 void fluid_motion::compute_advection(std::vector<vec3> & vertex_dis)
@@ -243,14 +248,14 @@ void fluid_motion::deform()
     
     double current_time = m_cur_global_idx + t;
     static double mile_stone = 0;
-//    if (current_time > mile_stone)
+    if (current_time > mile_stone)
     {
         profile_temp t("Projection");
         project_interface_one_iter();
-//        while (mile_stone < current_time)
-//        {
-//            mile_stone += 0.33; // Project three times at most in every particle load
-//        }
+        while (mile_stone < current_time)
+        {
+            mile_stone += 0.33; // Project three times at most in every particle load
+        }
     }
     
     static double mile_stone_log = 0;
@@ -446,10 +451,6 @@ double fluid_motion::project_interface()
     cout << "Max projection: " << max_displace << endl;
     
     int step = s_dsc->deform(20);
-    if (step > 6)
-    {
-        m_max_displacement_projection = m_max_displacement_projection * 6.0 / step;
-    }
 
     return max_displace;
 }
